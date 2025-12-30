d7424225c026733d7b43c139a36fc01afa4af6c3
nakiri5500 <nakiridaisuki5500@gmail.com>
2025-12-30 12:13:38 +0800

    init commit

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <glm/glm.hpp>
+
+int main(){
+
+    glm::vec3 test = { 1, 2, 3 };
+    std::cout << "Hello World" << std::endl;
+
+    std::cout << test.x << test.y << test.z << std::endl;
+
+    return 0;
+}
